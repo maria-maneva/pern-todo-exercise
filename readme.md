7bfcd53880318af4806083288dc336d6f7fe153f
Maria Maneva <maria.maneva@mentormate.com>
2021-06-21 14:17:16 +0300

    Updated readme md

--- readme.md
+++ readme.md
@@ -4,10 +4,12 @@
 
 `npm start` - starts both server and client
 
+To setup the database, run the initial commands in /server/src/database.sql in terminal
+
 To run separately:
 
 `npm run start:server`
 
 `npm run start:client`
 
-Both using typescript. Client uses bootstrap for styling
+Both using typescript. Client uses bootstrap for styling and context api for state management.
--- server/database.sql
+++ server/database.sql
@@ -9,13 +9,16 @@ CREATE TABLE todo(
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_DATE
 );
 
-ALTER TABLE todo DROP COLUMN date_created
-
-
-ALTER TABLE todo ADD COLUMN category_ids integer[]
-ALTER TABLE todo ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-
 CREATE TABLE category(
     cat_id SERIAL PRIMARY KEY,
     cat_name VARCHAR(255)
 );
+
+
+
+
+-- other commands, not needed ---
+ALTER TABLE todo DROP COLUMN date_created
+ALTER TABLE todo ADD COLUMN category_ids integer[]
+ALTER TABLE todo ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+
